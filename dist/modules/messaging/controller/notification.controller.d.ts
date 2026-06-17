020c71d143b995763c9568f75ba0da7dd5824c76
wolethescientist <davidaiyewumi1@gmail.com>
2026-06-17 10:40:23 +0100

    fix

--- dist/modules/messaging/controller/notification.controller.d.ts
+++ dist/modules/messaging/controller/notification.controller.d.ts
@@ -13,4 +13,4 @@ export declare class NotificationController {
     private _markAllRead;
     private _getQueueStats;
 }
-//# sourceMappingURL=notification.controller.d.ts.map
+//# sourceMappingURL=notification.controller.d.ts.map finihshed
